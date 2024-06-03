b76653951c775d2b358bcb4c7ba244317062f826
Dev00kushal <devkushal00@gmail.com>
2024-06-03 21:05:14 +0545

    chore: Refactor ApiServices to include a getRequest method

--- client/src/services/Api_services.ts
+++ client/src/services/Api_services.ts
@@ -1,4 +1,21 @@
-class ApiServices{
-// post get put patch delete
+import axios, { AxiosResponse } from 'axios';
 
+class ApiServices {
+    /**
+     * Makes a GET request using Axios.
+     * @param url - The URL to make the GET request to.
+     * @param params - Optional query parameters for the GET request.
+     * @returns A Promise with the Axios response.
+     */
+    public async getRequest<T>(url: string, params?: object): Promise<AxiosResponse<T>> {
+        try {
+            const response = await axios.get<T>(url, { params });
+            return response;
+        } catch (error) {
+            // Handle error (log, rethrow, etc.)
+            throw error;
+        }
+    }
 }
+
+export default ApiServices;
